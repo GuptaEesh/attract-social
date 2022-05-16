217a78464f23408e4ef1d19e13f63079d7c83206
GuptaEesh <p1r8a5t7ibha@gmail.com>
2022-05-16 17:27:14 +0530

    [chore] added routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { TopBar } from "./components";
+import { useAuth } from "./hooks";
+import { RedirectAuth } from "./route";
 import { Authenticate, LandingPage, UserProfile } from "./screens";
+import { appRoutes } from "./utils";
 
 function App() {
+  const {
+    state: { token },
+  } = useAuth();
   return (
     <div className={`${!true ? "dark" : ""} App`}>
-      {/* <Authenticate /> */}
-      {/* <LandingPage /> */}
-      {/* <TopBar /> */}
-      <UserProfile />
+      {token && <TopBar />}
+      <Routes>
+        <Route path={appRoutes.home} element={<LandingPage />} />
+        <Route element={<RedirectAuth />}>
+          <Route path={appRoutes.auth} element={<Authenticate />} />
+        </Route>
+        {/* <UserProfile /> */}
+      </Routes>
     </div>
   );
 }
