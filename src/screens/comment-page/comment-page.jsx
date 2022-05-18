054630c1d7c5df439c55b4169a01ab29632fcfa6
GuptaEesh <p1r8a5t7ibha@gmail.com>
2022-05-18 14:33:13 +0530

    [fix]: fixed bug for like

---
+++ src/screens/comment-page/comment-page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CommentOnPost = () => {
+  return (
+    <div className="flex flex-col items-center mb-8 mt-2 md:mt-2 lg:mt-24 w-[100vw] order-2 md:order-2 md:w-[100vw] lg:order-1 lg:w-[50vw]">
+      Comment here
+    </div>
+  );
+};
+
+export { CommentOnPost };
